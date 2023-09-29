f8d4dad4a321c372d3b84931e3965a3c523d3b07
羽末 <imyumo@outlook.com>
2023-09-29 16:57:29 +0800

    IQ Boost

--- bedwars-plugin/src/main/java/com/andrei1058/bedwars/listeners/bungee/StaffListener.java
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/listeners/bungee/StaffListener.java
@@ -23,7 +23,7 @@ public class StaffListener implements Listener {
         if (!player.hasPermission("minecraft.teleport.command")) return;
 
         String[] args = cmd.split(" ");
-        if (args.length >= 2) return;
+        if (args.length > 2) return;
         e.setCancelled(true);
         IArena arena = Arena.getArenaByPlayer(player);
         if (arena != null && arena.getStatus() == GameState.playing && !arena.isSpectator(player)) {
