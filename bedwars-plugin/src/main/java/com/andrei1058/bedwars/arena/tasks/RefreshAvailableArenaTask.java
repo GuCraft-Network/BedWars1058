847fda1ca3e1ad5416c7b073079939c565398890
羽末 <imyumo@outlook.com>
2023-09-29 15:13:53 +0800

    Update RefreshAvailableArenaTask.java

--- bedwars-plugin/src/main/java/com/andrei1058/bedwars/arena/tasks/RefreshAvailableArenaTask.java
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/arena/tasks/RefreshAvailableArenaTask.java
@@ -22,39 +22,30 @@ public class RefreshAvailableArenaTask implements Runnable {
         LinkedList<IArena> arenas = Arena.getArenas();
 
         if (arenas.isEmpty()) {
-            // 处理列表为空的情况
             availableArena = -1;
+            // 处理列表为空的情况
             return;
         }
 
         int maxPlayers = -1;
-        int smallestIndex = Integer.MAX_VALUE;
-        boolean arenaFound = false;
+        int maxPlayersIndex = -1;
 
         for (int i = 0; i < arenas.size(); i++) {
             IArena arena = arenas.get(i);
             int numPlayers = arena.getPlayers().size();
 
             if (arena.getStatus() != GameState.playing && arena.getStatus() != GameState.restarting) {
-                // 找到可用竞技场，设置标志并跳出循环
-                availableArena = i;
-                arenaFound = true;
-                break;
-            }
-
-            if (numPlayers > maxPlayers) {
-                // 当前竞技场人数更多，更新最大人数和最小索引
-                maxPlayers = numPlayers;
-                smallestIndex = i;
-            } else if (numPlayers == maxPlayers && i < smallestIndex) {
-                // 当前竞技场人数与最大人数相等，比较索引，取较小者
-                smallestIndex = i;
+                if (numPlayers > maxPlayers || numPlayers == maxPlayers && i < maxPlayersIndex) {
+                    // 找到人数最多的竞技场
+                    maxPlayers = numPlayers;
+                    maxPlayersIndex = i;
+                }
             }
         }
 
-        if (!arenaFound) {
-            // 如果没有找到可用竞技场，则使用最小索引
-            availableArena = smallestIndex;
+        if (maxPlayersIndex != -1) {
+            // 执行人数最多的竞技场的相关操作
+            availableArena = maxPlayersIndex;
         }
     }
 }
